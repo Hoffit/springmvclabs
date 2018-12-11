3a05473e785a69bb3aef71fc375247df346b9a4c
Hoffit <dehull_1@yahoo.com>
2018-12-10 21:54:13 -0800

    Initial Setup

---
+++ src/main/java/com/heally/springmvclabs/SpringmvclabsApplication.java
@@ -0,0 +1,12 @@
+package com.heally.springmvclabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvclabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvclabsApplication.class, args);
+	}
+}
