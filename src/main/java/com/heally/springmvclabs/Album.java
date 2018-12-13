a6a3ef46f6f4b7e011e0438091a9fa409e4c8fe2
Hoffit <dehull_1@yahoo.com>
2018-12-13 13:36:32 -0800

    database connected

--- src/main/java/com/heally/springmvclabs/Album.java
+++ src/main/java/com/heally/springmvclabs/Album.java
@@ -1,13 +1,21 @@
 package com.heally.springmvclabs;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * An album; a collection of musical songs.
  */
+@Entity
 public class Album {
 
     /**
      * A unique id for the album.
      */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     /**
@@ -37,6 +45,13 @@ public class Album {
     private String albumArtURL;
 
     /**
+     *
+     */
+    public Album() {
+        super();
+    }
+
+    /**
      * Constructor.
      * @param title
      * @param artist
@@ -49,6 +64,46 @@ public class Album {
         this.artist = artist;
         this.songCount = songCount;
         this.length = length;
+        this.albumArtURL = albumArtURL;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public int getSongCount() {
+        return songCount;
+    }
+
+    public void setSongCount(int songCount) {
+        this.songCount = songCount;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public String getAlbumArtURL() {
+        return albumArtURL;
+    }
+
+    public void setAlbumArtURL(String albumArtURL) {
         this.albumArtURL = albumArtURL;
     }
 }
---
+++ src/main/java/com/heally/springmvclabs/AlbumRepository.java
@@ -0,0 +1,9 @@
+package com.heally.springmvclabs;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ */
+public interface AlbumRepository extends CrudRepository<Album, Long> {
+}
